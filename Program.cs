09b5c9a70cf2e0cfc52b335689f4a5cff7d8f418
Milagro Scardigno <mdscardigno@gmail.com>
2022-08-30 19:03:32 -0400

    PromptForString Method

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 
 namespace EmployeeDatabase
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("  Welcome to Our Employee Database");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static string PromptForString(string prompt)
+        {
+            //use the argument, whatever the caller sent us
+            System.Console.WriteLine(prompt);
+            //get some user input
+            var userInput = Console.ReadLine();
+            //RETURN that value as the output of this method 
+            //the value in the 'userInput' will go wherever the 
+            //*CALLER* of the method has specified.
+            return userInput;
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
+            var name = PromptForString("What is your name? ");
+            var department = int.Parse(PromptForString("What is your department number? "));
+            var salary = int.Parse(PromptForString("What is your yearly salary (in dollar)?"));
+            var salaryPerMonth = salary / 12;
+            Console.WriteLine($"Hello, {name}, you make {salaryPerMonth} dollars per month.");
+        }
     }
-  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # EmployeeDatabase
+
+Name of Method: Display Greeting
+Input the method takes: None
+Work: Prints greeting to the console.
+Output: none.
+
+Name of Method: prompts for a string
+Input the method takes: The text from the prompt, as a string.
+Work: show the user the prompt - wait for their response.
+Output: the user's response, as a string.
