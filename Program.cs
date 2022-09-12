a5177a36414e61179c7c76204e9b5f09fd58cc3d
Milagro Scardigno <mdscardigno@gmail.com>
2022-09-12 16:21:07 -0400

    List of employees and other Employee constructor properties and attributes.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EmployeeDatabase
 {
@@ -6,7 +7,7 @@ namespace EmployeeDatabase
     //Class keyword
     //|
     //|
-    //|   Name of classes (PascalCase)
+    //|     Name of classes (PascalCase)
     //|    |
     //|    |
     //v    v
@@ -25,7 +26,7 @@ namespace EmployeeDatabase
         public int Department;
         public int Salary;
         public int MonthlySalary;
-
+        //The Properties of a class is the state that is keeping track of.   
         // This is a *special* method known as a "constructor"
         // The constructor is called when we write a line like: `var bob = new Employee(`
         // The arguments to the method should line up to those below
@@ -38,6 +39,12 @@ namespace EmployeeDatabase
         //              v               v                  v              v
         public Employee(string newName, int newDepartment, int newSalary, int newMonthlySalary)
         {
+            //there are other definitions for properties,whether they are public, private or protected.
+            //Properties can be accessed by code outside the class when properties are public.
+            //Private properties can only be seen from other code within the class itself. 
+            //Private properties...
+            //we cant use var style definition in the constructor since we must tell the code what type of information this property stores.
+            //Properties by convention are named camelCase.
             // In the constructor we should setup the values for any of the properties.
             // Here we will *copy* the values given by the arguments to the corresponding property.
             //- Property  Value
@@ -46,7 +53,7 @@ namespace EmployeeDatabase
             Name = newName;
             Department = newDepartment;
             Salary = newSalary;
-            MonthlySalary = newSalary;
+            MonthlySalary = newMonthlySalary;
         }
 
     }
@@ -105,6 +112,16 @@ namespace EmployeeDatabase
         }
         static void Main(string[] args)
         {
+
+            var graceHopper = new Employee("Grace Hopper", 100, 240_000, 20_000); // an Employee object
+            Console.WriteLine(graceHopper.Department);
+            var elonMusk = new Employee("Elon Musk", 42, 120_000, 10_000);
+            Console.WriteLine(elonMusk.Department);
+
+            var employees = new List<Employee>();
+            employees.Add(graceHopper);// first object in the list Index 0
+            employees.Add(elonMusk);// second object in the list Index 1
+
             DisplayGreeting();
             var name = PromptForString("What is your name? ");
             var department = PromptForInteger("What is your department number? ");
